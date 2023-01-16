940f637a956d8748465e99eced8ba4ac10f55113
bobcob7 <bobcob333@gmail.com>
2023-01-16 14:06:04 -0600

    Add auth assertion functions

--- pkg/auth/auth_check.go
+++ pkg/auth/auth_check.go
@@ -2,10 +2,13 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/ZacharyLangley/igru-web-server/pkg/context"
 	models "github.com/ZacharyLangley/igru-web-server/pkg/models/authentication"
 	authenticationv1 "github.com/ZacharyLangley/igru-web-server/pkg/proto/authentication/v1"
+	"github.com/ZacharyLangley/igru-web-server/pkg/proto/authentication/v1/authenticationv1connect"
+	"github.com/bufbuild/connect-go"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
 )
@@ -25,6 +28,61 @@ func GetGroups(ctx context.Context, tx pgx.Tx, userID uuid.UUID) (map[uuid.UUID]
 		groupIDs[group.ID] = struct{}{}
 	}
 	return groupIDs, nil
+}
+
+type Checker struct {
+	authenticationv1connect.SessionServiceClient
+}
+
+func (c Checker) AssertAll(ctx context.Context, req interface {
+	Header() http.Header
+}, groupID string, roles ...authenticationv1.GroupRole) (bool, error) {
+	request := connect.NewRequest(&authenticationv1.CheckSessionPermissionsRequest{})
+	if len(roles) == 0 {
+		return false, nil
+	}
+	request.Msg.Requests = make([]*authenticationv1.PermissionRequest, len(roles))
+	for i, role := range roles {
+		request.Msg.Requests[i] = &authenticationv1.PermissionRequest{
+			GroupId: groupID,
+			Role:    role,
+		}
+	}
+	res, err := c.SessionServiceClient.CheckSessionPermissions(ctx, request)
+	if err != nil {
+		return false, err
+	}
+	for _, response := range res.Msg.Responses {
+		if !response.Allowed {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+func (c Checker) AssertAny(ctx context.Context, req interface {
+	Header() http.Header
+}, groupID string, roles ...authenticationv1.GroupRole) (bool, error) {
+	request := connect.NewRequest(&authenticationv1.CheckSessionPermissionsRequest{})
+	if len(roles) == 0 {
+		return false, nil
+	}
+	request.Msg.Requests = make([]*authenticationv1.PermissionRequest, len(roles))
+	for i, role := range roles {
+		request.Msg.Requests[i] = &authenticationv1.PermissionRequest{
+			GroupId: groupID,
+			Role:    role,
+		}
+	}
+	res, err := c.SessionServiceClient.CheckSessionPermissions(ctx, request)
+	if err != nil {
+		return false, err
+	}
+	for _, response := range res.Msg.Responses {
+		if !response.Allowed {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func Check(ctx context.Context, tx pgx.Tx, userID uuid.UUID, requests []*authenticationv1.PermissionRequest) ([]*authenticationv1.PermissionResponse, error) {
