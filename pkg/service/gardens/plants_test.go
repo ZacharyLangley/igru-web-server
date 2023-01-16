f54ae04966488425ed2eb3a535849f1533401737
Zachary L <zacharylangley@my.unt.edu>
2023-01-16 11:38:12 -0600

    Revert "ZL | Database Setup For Testing"

--- pkg/service/gardens/plants_test.go
+++
@@ -1,9 +0,0 @@
-package gardens_test
-
-import "testing"
-
-func TestCreatePlant(t *testing.T) {}
-func TestUpdatePlant(t *testing.T) {}
-func TestGetPlant(t *testing.T)    {}
-func TestGetPlants(t *testing.T)   {}
-func TestDeletePlant(t *testing.T) {}
--- pkg/service/gardens/recipes_test.go
+++
@@ -1,9 +0,0 @@
-package gardens_test
-
-import "testing"
-
-func TestCreateRecipe(t *testing.T) {}
-func TestUpdateRecipe(t *testing.T) {}
-func TestGetRecipe(t *testing.T)    {}
-func TestGetRecipes(t *testing.T)   {}
-func TestDeleteRecipe(t *testing.T) {}
