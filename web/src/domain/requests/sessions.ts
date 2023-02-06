0099d22cd1eebeeeceaf7c0f0297b4bf19b3e46b
Zachary Langley <zaclangley04@gmail.com>
2023-02-06 10:50:44 -0600

    ZL | Auth Service Request Framework

---
+++ web/src/domain/requests/sessions.ts
@@ -0,0 +1,15 @@
+export const signInRequest = async () => {
+    return;
+}
+
+export const signOutRequest = async () => {
+    return;
+}
+
+export const validateSessionRequest = async () => {
+    return;
+}
+
+export const validateSessionPermissions = async () => {
+    return;
+}
--- web/src/domain/requests/user.ts
+++ web/src/domain/requests/user.ts
@@ -1,1 +1,19 @@
-export {};
+export const signUpRequest = async () => {
+    return
+}
+
+export const deleteUserRequest = async () => {
+    return;
+}
+
+export const updateUserRequest = async () => {
+    return;
+}
+
+export const getUsersRequest = async () => {
+    return;
+}
+
+export const resetPasswordRequest = async () => {
+    return;
+}
