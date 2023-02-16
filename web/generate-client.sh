716c467bab7762d9846240c181b2b37fb22376e0
bobcob7 <bobcob333@gmail.com>
2023-02-15 23:14:45 -0600

    Remove extra buf stuff

--- web/generate-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /home/$USER/.nvm/nvm.sh
-echo "Running Buf Generate..."
-npm run buf:generate
-echo "Cleaning up..."
-sudo rm -rf ./apis
-echo "Generation Complete. Don't lose your mind too quickly!"
